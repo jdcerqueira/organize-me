f3e41696d79983e991af4e774c517192618a5d09
João Cerqueira <jd_cerqueira@yahoo.com.br>
2022-07-08 22:00:48 -0300

    Criação da lista de CEPs vinculados na base e inserção de um novo CEP

--- cadastro-services/Application/endereco_dao.py
+++ cadastro-services/Application/endereco_dao.py
@@ -1,27 +1,29 @@
 import logging
+from msilib.schema import Error
 import pyodbc
 from config.properties import Properties
 import requests
 
 class EnderecoDAO():
-    def __init__(self, cep):
+    def __init__(self, cep=None):
         cfg = Properties("ORGANIZEDB02").properties
         self.connection = pyodbc.connect(cfg["datasource"])
         self.cursor = self.connection.cursor()
 
-        self.cep = cep
-        if self.validateStreetByCep() == None:
-            cep_request = requests.get(f"http://viacep.com.br/ws/{self.cep}/json")
-            if "erro" in cep_request.json():
-                print("erro: " + cep_request.json()["erro"])
-            else:
-                self.nome = str(cep_request.json()["logradouro"]).upper()
-                self.complemento = str(cep_request.json()["complemento"]).upper()
-                self.bairro = str(cep_request.json()["bairro"]).upper()
-                self.cidade = str(cep_request.json()["localidade"]).upper()
-                self.uf = str(cep_request.json()["uf"]).upper()
-                self.pais = "BRASIL"
-                self.validateStreetByDistrict(self.validateDistrict(self.validateCity(self.validateState(self.validateCountry()))))
+        if cep != None:
+            self.cep = cep
+            if self.validateStreetByCep() == None:
+                cep_request = requests.get(f"http://viacep.com.br/ws/{self.cep}/json")
+                if "erro" in cep_request.json():
+                    print("erro: " + cep_request.json()["erro"])
+                else:
+                    self.nome = str(cep_request.json()["logradouro"]).upper()
+                    self.complemento = str(cep_request.json()["complemento"]).upper()
+                    self.bairro = str(cep_request.json()["bairro"]).upper()
+                    self.cidade = str(cep_request.json()["localidade"]).upper()
+                    self.uf = str(cep_request.json()["uf"]).upper()
+                    self.pais = "BRASIL"
+                    self.validateStreetByDistrict(self.validateDistrict(self.validateCity(self.validateState(self.validateCountry()))))
 
     def validateCountry(self):
         try:
@@ -114,6 +116,62 @@ class EnderecoDAO():
         except pyodbc.Error as ex:
             print(self.exceptions(ex))
             return None
+
+    def lista(self):
+        retorno = []
+
+        try:
+            self.cursor.execute("SELECT pais, uf, estado, cidade, bairro, cep, logradouro, complemento FROM app.vEnderecos ORDER BY cep")
+            row = self.cursor.fetchone()
+            while row:
+                retorno.append(
+                    {
+                        "cep":row[5],
+                        "logradouro":row[6],
+                        "complemento":row[7],
+                        "bairro":row[4],
+                        "cidade":row[3],
+                        "estado":row[2],
+                        "uf":row[1],
+                        "pais":row[0]
+                    }
+                )
+                row = self.cursor.fetchone()
+
+            if len(retorno):
+                return {"return":retorno, "http_state": 200}
+            
+            return {"return":retorno, "http_state": 204}
+        except pyodbc.Error as ex:
+            return {"return":self.exceptions(ex), "http_state":500}
+
+    def listaEspec(self,cep):
+        retorno = []
+
+        try:
+            self.cursor.execute("SELECT pais, uf, estado, cidade, bairro, cep, logradouro, complemento FROM app.vEnderecos WHERE cep = '"+ cep +"'")
+            row = self.cursor.fetchone()
+            while row:
+                retorno.append(
+                    {
+                        "cep":row[5],
+                        "logradouro":row[6],
+                        "complemento":row[7],
+                        "bairro":row[4],
+                        "cidade":row[3],
+                        "estado":row[2],
+                        "uf":row[1],
+                        "pais":row[0]
+                    }
+                )
+                row = self.cursor.fetchone()
+
+            if len(retorno):
+                return {"return":retorno, "http_state": 200}
+            
+            return {"return":retorno, "http_state": 204}
+        except pyodbc.Error as ex:
+            return {"return":self.exceptions(ex), "http_state":500}
         
     def exceptions(self, ex):
         logging.error(ex.args[1])
--- cadastro-services/Application/endpoints.py
+++ cadastro-services/Application/endpoints.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from genero_srv import Generos
 from responsavel_srv import Responsaveis
 from categoria_srv import Categoria
+from endereco_srv import Endereco
 
 
 app = Flask(__name__)
@@ -19,6 +20,9 @@ api.add_resource(Responsaveis, "/cadastro/responsaveis/apaga/<string:responsavel
 api.add_resource(Categoria, "/cadastro/categorias/lista", endpoint="lista_categoria")
 api.add_resource(Categoria, "/cadastro/categorias/insere/<string:nome>", endpoint="insere_categoria")
 api.add_resource(Categoria, "/cadastro/categorias/apaga/<string:categoria>", endpoint="apaga_categoria")
+
+api.add_resource(Endereco, "/cadastro/enderecos/lista", endpoint="lista_endereco")
+api.add_resource(Endereco, "/cadastro/enderecos/insere/<string:cep>", endpoint="insere_endereco")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8081)
